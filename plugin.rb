ec179e2b0e9642dc820ffd91899e21758b18511c
Marcos <juangura@gmail.com>
2024-12-05 12:00:28 -0400

    DEV: Initial commit

---
+++ plugin.rb
@@ -0,0 +1,5 @@
+# name: discourse-live-search-plugin
+# about: A plugin to integrate typesense search engine to Discourse
+# version: 0.0.1
+# authors: Marcos Gutierrez
+# url: https://github.com/yourusername/basic-plugin
